5d90292be65667bdebc9fa28978598b7dc226f44
xiaobin <bill_xiaobin@163.com>
2018-01-23 23:39:29 +0800

    api test

--- app/Http/Controllers/Api/MusicController.php
+++ app/Http/Controllers/Api/MusicController.php
@@ -10,9 +10,9 @@ class MusicController extends Controller
     public function musiclist()
     {
         $list = [
-            ["urls"=>"http://vip.baidu190.com/uploads/2017/20170207ff925ef6f268a5563f0552c3ba141f.mp3","author"=>"杨宗纬","name"=>"凉凉"],
-            ["urls"=>"http://vip.baidu190.com/uploads/2017/2017102c57e85460e4e9ee576f90583a40236b.mp3","author"=>"陈一发","name"=>"童话镇"],
-            ["urls"=>"http://vip.baidu190.com/uploads/2017/2016076a16a68ba55e721878650753d0c3256a.mp3","author"=>"袁娅维","name"=>"说散就散"]
+            ["urls"=>"http://vip.baidu190.com/uploads/2017/20170207ff925ef6f268a5563f0552c3ba141f.mp3","author"=>"杨宗纬","name"=>"凉凉","img"=>"http://i.gtimg.cn/music/photo/mid_album_90/a/F/000QgFcm0v8WaF.jpg"],
+            ["urls"=>"http://vip.baidu190.com/uploads/2017/2017102c57e85460e4e9ee576f90583a40236b.mp3","author"=>"陈一发","name"=>"童话镇","img"=>"http://i.gtimg.cn/music/photo/mid_album_90/a/F/000QgFcm0v8WaF.jpg"],
+            ["urls"=>"http://vip.baidu190.com/uploads/2017/2016076a16a68ba55e721878650753d0c3256a.mp3","author"=>"袁娅维","name"=>"说散就散","img"=>"http://i.gtimg.cn/music/photo/mid_album_90/a/F/000QgFcm0v8WaF.jpg"]
         ];
         fun_respon(200, $list);
     }
